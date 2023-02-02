3ca02389f293299f87821731564536261b5b3e2c
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-02-02 15:43:21 +0200

    component edit styles

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -4,14 +4,14 @@ import {ReactComponent as SearchIcon} from '@/assets/images/svg/search.svg';
 
 const Search = () => {
   return (
-    <label className="flex items-center rounded-full h-full px-3 bg-gray-100 text-gray-500 w-60">
-      <span className="mr-2">
-        <SearchIcon />
+    <label className="flex items-center rounded-full h-full px-3 bg-gray-100 w-60">
+      <span>
+        <SearchIcon className="text-gray-500" />
       </span>
       <input
         type="text"
         placeholder="Поиск на Facebook"
-        className="w-full h-full py-2 bg-transparent text-sm focus:outline-none"
+        className="w-full h-full p-2 bg-transparent text-[15px] focus:outline-none text-text-primary"
       />
     </label>
   );
